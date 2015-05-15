65d3d44d9344c07288afb897620ece48b4edca36
Alin Gherasim <agherasim@luxoft.com>
2015-05-15 13:14:09 +0300

    Initial version of the metrics viewer.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # metrics-viewer
+
+## Requirements
+In order to build the project you need to have Maven 3.x installed.
+
+In order to run it you need to have JDK 8
+
+## Usage
+To build the project execute the 'mvn clean install' command.
+
+To run the project execute 'java -jar example.agrona-0.0.1-SNAPSHOT-jar-with-dependencies.jar' command.
---
+++ src/main/java/org/kaazing/example/agrona/MetricsViewer.java
@@ -0,0 +1,50 @@
+package org.kaazing.example.agrona;
+
+import java.io.File;
+import java.nio.MappedByteBuffer;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import uk.co.real_logic.agrona.IoUtil;
+import uk.co.real_logic.agrona.concurrent.CountersManager;
+import uk.co.real_logic.agrona.concurrent.SigInt;
+import uk.co.real_logic.agrona.concurrent.UnsafeBuffer;
+
+public class MetricsViewer {
+
+    private static int COUNTER_LABELS_BUFFER_LENGTH = 32 * 1024 * 1024;
+    private static int COUNTER_VALUES_BUFFER_LENGTH = 1024 * 1024;
+
+    public static void main(String[] args) throws InterruptedException {
+        File tmpDir = new File(IoUtil.tmpDirName() + "\\kaazing", "monitor");
+        MappedByteBuffer mapNewFile = IoUtil.mapExistingFile(tmpDir, "monitor");
+
+        UnsafeBuffer labelsBuffer = new UnsafeBuffer(mapNewFile, 64, COUNTER_LABELS_BUFFER_LENGTH);
+        UnsafeBuffer valuesBuffer =
+                new UnsafeBuffer(mapNewFile, 64 + COUNTER_LABELS_BUFFER_LENGTH, COUNTER_VALUES_BUFFER_LENGTH);
+
+        CountersManager countersManager = new CountersManager(labelsBuffer, valuesBuffer);
+
+        final AtomicBoolean running = new AtomicBoolean(true);
+        SigInt.register(() -> running.set(false));
+
+        while (running.get()) {
+            System.out.println("Agrona counters:");
+            System.out.format("%1$tH:%1$tM:%1$tS - Gateway monitor\n", new Date());
+            System.out.println("=========================");
+            System.out.println("Counter id: Counter value - Counter name");
+
+            countersManager.forEach((id, label) -> {
+                final int offset = CountersManager.counterOffset(id);
+                final long value = valuesBuffer.getLongVolatile(offset);
+
+                System.out.format("%3d: %,20d - %s\n", id, value, label);
+            });
+
+            System.out.println("");
+
+            Thread.sleep(1000);
+        }
+    }
+
+}
